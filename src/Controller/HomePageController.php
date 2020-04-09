9de138e8c9d83b830d76888a6d346ce27c40d6b7
Dawid <dawidbury1998@gmail.com>
2020-04-09 20:11:20 +0200

    Add homepage and install twig bundle

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,17 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class IndexController extends AbstractController
+class HomePageController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home_page")
      */
     public function index()
     {
-        return new JsonResponse('test', 200);
+        return $this->render('default/homepage.html.twig');
     }
 }
